f8bd35fea99abc152a2c8c6119f6762741564a25
Subhash4574 <amazingsubhash07@gmail.com>
2023-01-15 21:52:08 +0100

    requst mapping changed

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -3,10 +3,12 @@ package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/Subh")
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
